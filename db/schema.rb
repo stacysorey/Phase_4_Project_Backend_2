713a41ccbc180c0b5a066acb0d2c9611a9864ac1
stacysorey <stacysorey@gmail.com>
2021-09-15 23:57:56 -0400

    update sschema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_041952) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "entries", force: :cascade do |t|
     t.string "title"

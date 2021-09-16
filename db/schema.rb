6f04125e3c36a8e639343191da20ffe0b5a262a6
stacysorey <stacysorey@gmail.com>
2021-09-15 23:20:34 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_08_041952) do
 
+  create_table "entries", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "date"
+    t.integer "journal_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["journal_id"], name: "index_entries_on_journal_id"
+  end
+
+  create_table "journals", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "entries", "journals"
 end
